1eebd6444b2667718623816ed80bb5453639921a
Cade Hancock <93878207+matthewcade@users.noreply.github.com>
2026-04-09 22:32:08 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import GridGame from './components/gridgame.tsx';
+import Logo from './images/logo.png';
 
 function App() {
   return (
     <>
+      <header>
+        <img src={Logo} width="200px" />
+      </header>
       <GridGame />
     </>
   )
