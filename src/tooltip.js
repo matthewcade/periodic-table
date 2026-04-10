e3ec3546a31faf5076b4cb4275be642291e35357
Cade Hancock <93878207+matthewcade@users.noreply.github.com>
2026-04-09 22:27:05 -0500

    Delete src/tooltip.js
    Defunct

--- src/tooltip.js
+++
@@ -1,6 +0,0 @@
-const tooltipImage = document.querySelector("#tooltip-image");
-const tooltip = document.querySelector("#tooltip");
-
-tooltipImage.addEventListener("mouseenter", function() {
-    tooltip.style.opacity = "1";
-})
