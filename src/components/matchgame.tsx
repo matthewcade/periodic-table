e6b78bb93a49b0fc76731edbf73d32cc8a65159c
Cade Hancock <93878207+matthewcade@users.noreply.github.com>
2026-07-03 16:12:58 -0500

    Match Game Enhancement
    + Elements are hidden until selected
    + Corrected the pre-shuffle so elements are randomly placed on page load

--- src/components/matchgame.tsx
+++ src/components/matchgame.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '../styles/matchgame.module.css';
 import elements from '../assets/elements.tsx';
+import frontCard from '../images/front card.png';
 
 const allElements = Object.values(elements);
 let choices: any = [];
@@ -10,6 +11,7 @@ let randomizedElements = randomizeSymbols.concat([...randomizeNames]);
 let selection: any = [];
 let firstChoice = '';
 let firstChoiceId = '';
+let firstChoiceFront = '';
 
 function generateElements(e: any) {
     choices = [];
@@ -58,21 +60,70 @@ function generateElements(e: any) {
     randomizedElements = randomizeSymbols.concat([...randomizeNames]);
 }
 
-function handleSelection(e: any, id: any) {
+function flipFront(e: any) {
+    if (document.getElementById(e)?.classList.contains(styles.unflip)) {
+        document.getElementById(e)?.classList.replace(styles.unflip, styles.flip);
+    } else {
+        document.getElementById(e)?.classList.add(styles.flip);
+    }
+}
+
+function flipBack(e: any) {
+    if (document.getElementById(e)?.classList.contains(styles.flip)) {
+        document.getElementById(e)?.classList.replace(styles.flip, styles.unflip);
+    } else {
+        document.getElementById(e)?.classList.add(styles.unflip);
+    }
+}
+
+function unflip(first: any, second: any) {
+    document.getElementById(first)?.classList.replace(styles.flip, styles.unflip);
+    document.getElementById(second)?.classList.replace(styles.unflip, styles.flip);
+}
+
+function enableButtons() {
+    const allButtons = document.querySelectorAll(`.${styles.frontcard}`);
+
+    allButtons.forEach(button => {
+        button.removeAttribute('disabled');
+    })
+}
+
+function disableButtons() {
+    const allButtons = document.querySelectorAll(`.${styles.frontcard}`);
+
+    allButtons.forEach(button => {
+        button.setAttribute('disabled', 'true');
+    })
+
+    setTimeout(enableButtons, 4000);
+}
+
+function handleSelection(e: any, id: any, front: any) {
+    flipFront(front);
+    console.log(e, id, front);
+
     if (firstChoice == '') {
         firstChoice = e;
         firstChoiceId = id;
+        firstChoiceFront = front;
+
+        flipBack(id);
     } else if (firstChoice == e) {
-        console.log('Correct!');
-        document.getElementById(id)?.classList.add(styles.correctanswer);
-        document.getElementById(firstChoiceId)?.classList.add(styles.correctanswer);
+        flipBack(id);
+
         firstChoice = '';
         firstChoiceId = '';
     } else {
-        console.log('Incorrect...');
-        console.log(firstChoice, e);
+        flipBack(id);
+
+        setTimeout(unflip, 3000, front, id);
+        setTimeout(unflip, 3000, firstChoiceFront, firstChoiceId);
+
         firstChoice = '';
         firstChoiceId = '';
+
+        disableButtons();
     }
 }
 
@@ -131,10 +182,10 @@ const MatchGame = () => {
     }
 
     function resetClasses() {
-        const correctAnswers = document.querySelectorAll(`.${styles.correctanswer}`);
+        const flippedCards = document.querySelectorAll(`.${styles.flip}`);
 
-        correctAnswers.forEach(correctAnswer => {
-            correctAnswer.classList.remove(`${styles.correctanswer}`);
+        flippedCards.forEach(flippedCard => {
+            flippedCard.classList.remove(`${styles.flip}`);
         })
     }
 
@@ -175,26 +226,110 @@ const MatchGame = () => {
         setValueSixteen(selection[15][1]);
     }
 
+    useEffect(() => {
+        setElements();
+    }, []);
+
     return (
         <div id={styles.container}>
             <h1 id={styles.instructions}>Match the element to the symbol</h1>
             <div id={styles.gamegrid}>
-                <button id={`${blockOne}`} className={styles.gamebutton} value={valueOne} onClick={() => handleSelection(`${valueOne}`, `${blockOne}`)}>{blockOne}</button>
-                <button id={`${blockTwo}`} className={styles.gamebutton} value={valueTwo} onClick={() => handleSelection(`${valueTwo}`, `${blockTwo}`)}>{blockTwo}</button>
-                <button id={`${blockThree}`} className={styles.gamebutton} value={valueThree} onClick={() => handleSelection(`${valueThree}`, `${blockThree}`)}>{blockThree}</button>
-                <button id={`${blockFour}`} className={styles.gamebutton} value={valueFour} onClick={() => handleSelection(`${valueFour}`, `${blockFour}`)}>{blockFour}</button>
-                <button id={`${blockFive}`} className={styles.gamebutton} value={valueFive} onClick={() => handleSelection(`${valueFive}`, `${blockFive}`)}>{blockFive}</button>
-                <button id={`${blockSix}`} className={styles.gamebutton} value={valueSix} onClick={() => handleSelection(`${valueSix}`, `${blockSix}`)}>{blockSix}</button>
-                <button id={`${blockSeven}`} className={styles.gamebutton} value={valueSeven} onClick={() => handleSelection(`${valueSeven}`, `${blockSeven}`)}>{blockSeven}</button>
-                <button id={`${blockEight}`} className={styles.gamebutton} value={valueEight} onClick={() => handleSelection(`${valueEight}`, `${blockEight}`)}>{blockEight}</button>
-                <button id={`${blockNine}`} className={styles.gamebutton} value={valueNine} onClick={() => handleSelection(`${valueNine}`, `${blockNine}`)}>{blockNine}</button>
-                <button id={`${blockTen}`} className={styles.gamebutton} value={valueTen} onClick={() => handleSelection(`${valueTen}`, `${blockTen}`)}>{blockTen}</button>
-                <button id={`${blockEleven}`} className={styles.gamebutton} value={valueEleven} onClick={() => handleSelection(`${valueEleven}`, `${blockEleven}`)}>{blockEleven}</button>
-                <button id={`${blockTwelve}`} className={styles.gamebutton} value={valueTwelve} onClick={() => handleSelection(`${valueTwelve}`, `${blockTwelve}`)}>{blockTwelve}</button>
-                <button id={`${blockThirteen}`} className={styles.gamebutton} value={valueThirteen} onClick={() => handleSelection(`${valueThirteen}`, `${blockThirteen}`)}>{blockThirteen}</button>
-                <button id={`${blockFourteen}`} className={styles.gamebutton} value={valueFourteen} onClick={() => handleSelection(`${valueFourteen}`, `${blockFourteen}`)}>{blockFourteen}</button>
-                <button id={`${blockFifteen}`} className={styles.gamebutton} value={valueFifteen} onClick={() => handleSelection(`${valueFifteen}`, `${blockFifteen}`)}>{blockFifteen}</button>
-                <button id={`${blockSixteen}`} className={styles.gamebutton} value={valueSixteen} onClick={() => handleSelection(`${valueSixteen}`, `${blockSixteen}`)}>{blockSixteen}</button>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontone} className={styles.frontcard} value={valueOne} onClick={() => { handleSelection(`${valueOne}`, `${blockOne}`, `${styles.frontone}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockOne}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockOne}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.fronttwo} className={styles.frontcard} value={valueTwo} onClick={() => { handleSelection(`${valueTwo}`, `${blockTwo}`, `${styles.fronttwo}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockTwo}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockTwo}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontthree} className={styles.frontcard} value={valueThree} onClick={() => { handleSelection(`${valueThree}`, `${blockThree}`, `${styles.frontthree}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockThree}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockThree}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontfour} className={styles.frontcard} value={valueFour} onClick={() => { handleSelection(`${valueFour}`, `${blockFour}`, `${styles.frontfour}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockFour}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockFour}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontfive} className={styles.frontcard} value={valueFive} onClick={() => { handleSelection(`${valueFive}`, `${blockFive}`, `${styles.frontfive}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockFive}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockFive}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontsix} className={styles.frontcard} value={valueSix} onClick={() => { handleSelection(`${valueSix}`, `${blockSix}`, `${styles.frontsix}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockSix}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockSix}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontseven} className={styles.frontcard} value={valueSeven} onClick={() => { handleSelection(`${valueSeven}`, `${blockSeven}`, `${styles.frontseven}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockSeven}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockSeven}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.fronteight} className={styles.frontcard} value={valueEight} onClick={() => { handleSelection(`${valueEight}`, `${blockEight}`, `${styles.fronteight}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockEight}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockEight}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontnine} className={styles.frontcard} value={valueNine} onClick={() => { handleSelection(`${valueNine}`, `${blockNine}`, `${styles.frontnine}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockNine}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockNine}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontten} className={styles.frontcard} value={valueTen} onClick={() => { handleSelection(`${valueTen}`, `${blockTen}`, `${styles.frontten}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockTen}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockTen}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.fronteleven} className={styles.frontcard} value={valueEleven} onClick={() => { handleSelection(`${valueEleven}`, `${blockEleven}`, `${styles.fronteleven}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockEleven}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockEleven}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.fronttwelve} className={styles.frontcard} value={valueTwelve} onClick={() => { handleSelection(`${valueTwelve}`, `${blockTwelve}`, `${styles.fronttwelve}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockTwelve}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockTwelve}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontthirteen} className={styles.frontcard} value={valueThirteen} onClick={() => { handleSelection(`${valueThirteen}`, `${blockThirteen}`, `${styles.frontthirteen}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockThirteen}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockThirteen}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontfourteen} className={styles.frontcard} value={valueFourteen} onClick={() => { handleSelection(`${valueFourteen}`, `${blockFourteen}`, `${styles.frontfourteen}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockFourteen}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockFourteen}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontfifteen} className={styles.frontcard} value={valueFifteen} onClick={() => { handleSelection(`${valueFifteen}`, `${blockFifteen}`, `${styles.frontfifteen}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockFifteen}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockFifteen}</button>
+                </div>
+                <div className={styles.blockcontainer}>
+                    <button id={styles.frontsixteen} className={styles.frontcard} value={valueSixteen} onClick={() => { handleSelection(`${valueSixteen}`, `${blockSixteen}`, `${styles.frontsixteen}`); }}>
+                        <img src={frontCard} />
+                    </button>
+                    <button id={`${blockSixteen}`} className={`${styles.gamebutton} ${styles.backcard}`} disabled>{blockSixteen}</button>
+                </div>
             </div>
             <button id={styles.reshuffle} onClick={() => { setElements(); resetClasses(); }}>Reshuffle</button>       
         </div>
